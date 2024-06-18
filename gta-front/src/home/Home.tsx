b657e3405c27bb2fb5d53f9cec0e2965c5e6e002
MattKenyei <mattkenyei@gmail.com>
2024-06-19 02:15:10 +0300

    added react-hook-form

--- gta-front/src/home/Home.tsx
+++ gta-front/src/home/Home.tsx
@@ -1,16 +1,23 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import styles from "./Home.module.css";
-const isSuccess = true;
+import { IHome } from "./ihome";
+const isSuccess = false;
+
 function Home() {
+  const {register, handleSubmit} = useForm<IHome>()
+  const onSubmit : SubmitHandler<IHome> = (data) => {
+    console.log(data)
+  } 
   return (
     <div className={styles.wrapper}>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
       {isSuccess ? (
         <div className={styles.success}>The form has been sent successful</div>
       ) : (
         <>
           <h1>GTA VI</h1>
-          <input type="email" placeholder="Input Email: " />
-          <input type="name" placeholder="Input your name: " />
+          <input type="email" placeholder="Input Email: " {...register('email')}/>
+          <input type="name" placeholder="Input your name: " {...register('name')}/>
           <button>Want GTA!</button>
           </>
           )}
